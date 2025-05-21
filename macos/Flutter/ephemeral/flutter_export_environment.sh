5e8b6c52d47ddd40d3b670f9e51090bfd5776382
yasirzain15 <softwareguy1515@gmail.com>
2025-05-22 00:01:55 +0500

    Task Completed

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter_windows_3.27.1-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\flutter2025\qofi-driver-app"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\qofi-driver-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
